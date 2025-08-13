de8e11da448906c21382b064aa8ca7a97c8b0b2a
Eduardo-B-Oliveira <167372580+Eduardo-B-Oliveira@users.noreply.github.com>
2025-08-13 13:20:53 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,6 +3,7 @@ const urlsToCache = [
     '/pwa1308/',
     '/pwa1308/index.html',
     '/pwa1308/style.css',
+    '/pwa1308/script.js',
     '/pwa1308/manifest.json',
     '/pwa1308/icons/icon-192x192.png',
     '/pwa1308/icons/icon-512x512.png'
